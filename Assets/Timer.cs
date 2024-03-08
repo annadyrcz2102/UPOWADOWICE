d6ed9c10ef53d9b89275b4c32832fc608d6fcee0
Konrad <konrad9smolik@outlook.com>
2024-03-08 19:38:21 +0100

    timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Timer : MonoBehaviour , IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Pause = !Pause;
+    }
+
+    [SerializeField] private Image uiFill;
+    [SerializeField] private Text uiText;
+
+    public int Duration;
+
+    private int remainingDuration;
+
+    private bool Pause;
+
+    private void Start()
+    {
+        Begin(Duration);
+    }
+
+    private void Begin(int second)
+    {
+        remainingDuration = second;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (remainingDuration >= 0)
+        {
+            if (!Pause)
+            {
+                uiText.text = $"{remainingDuration % 60:00}";
+                uiFill.fillAmount = Mathf.InverseLerp(0, Duration, remainingDuration);
+                remainingDuration--;
+                yield return new WaitForSeconds(1f);
+            }
+            yield return null;
+        }
+        OnEnd();
+    }
+
+    private void OnEnd()
+    {
+        Debug.Log("End");
+    }
+}
